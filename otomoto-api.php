57fa1b4aa092eaf22d0a45c653f6ae5b1a36d087
Norbert Jurga <jurganorbert@gmail.com>
2021-01-17 22:26:13 +0100

    update requests 1/x

--- otomoto-api.php
+++ otomoto-api.php
@@ -9,7 +9,7 @@ Author: Tojekmek
 Author URI: https://tojekmek.pl
 */
 
-include( plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php');
+include(plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php');
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/tojekmek-pl/otomoto-api',
 	__FILE__,
@@ -142,14 +142,165 @@ class PageTemplaterImport
 
 		add_action('edit_form_top', 'add_custom_form');
 
-		function add_custom_form()
-		{
+		if (isset($_POST['post_id'])) {
+			$post_id = $_POST['post_id'];
 
-			global $current_screen;
-			if ('samochod' != $current_screen->post_type) {
-				return;
+			$otomoto_id = get_post_meta($_POST['post_id'], 'otomoto_id');
+
+			$long = get_field('samochod_lokalizacja', $post_id) == 'gliwice' ? "18.58702" : "19.06848";
+			$latit = get_field('samochod_lokalizacja', $post_id) == 'gliwice' ? "50.33793" : "50.22196";
+
+			$region_id = "6";
+			$city_id = get_field('samochod_lokalizacja', $post_id) == 'gliwice' ? "6091" : "7691";
+			$district_id = get_field('samochod_lokalizacja', $post_id) == 'gliwice' ? "181" : "229";
+
+			$municipality = ucfirst(get_field('samochod_lokalizacja', $post_id));
+
+			$paint_type = get_field('samochod_kolor_typ', $post_id);
+
+			$explodedMake = explode('|', get_field('samochod_marka', $post_id));
+
+			$make = $explodedMake[1];
+			$model = $explodedMake[2];
+			$version = $explodedMake[3] ?? '';
+
+			$city = [
+				'pl' => ucfirst(get_field('samochod_lokalizacja', $post_id)),
+				'en' => ucfirst(get_field('samochod_lokalizacja', $post_id))
+			];
+			$district = [
+				'pl' => ucfirst(get_field('samochod_lokalizacja', $post_id)),
+				'en' => ucfirst(get_field('samochod_lokalizacja', $post_id))
+			];
+
+			$params = [
+				"make" => $make,
+				"model" => $model,
+				"version" => $version,
+				"year" => get_field('samochod_rok', $post_id),
+				"mileage" => get_field('samochod_przebieg', $post_id),
+				"engine_capacity" => get_field('samochod_pojemnosc', $post_id),
+				"fuel_type" => get_field('samochod_paliwo', $post_id),
+				"engine_power" => get_field('samochod_moc', $post_id),
+				"gearbox" => get_field('samochod_skrzynia', $post_id),
+				"transmission" => get_field('samochod_naped', $post_id),
+				'no_accident' => get_field('samochod_bezkolizyjny', $post_id),
+				"door_count" => get_field('samochod_liczba_drzwi', $post_id),
+				"nr_seats" => get_field('samochod_liczba_miejsc', $post_id),
+				"color" => get_field('samochod_kolor', $post_id),
+				$paint_type => 1,
+				"vat_discount" => get_field('samochod_fvmarza', $post_id),
+				"financial_option" => get_field('samochod_finansowanie', $post_id),
+				"vat" => get_field('samochod_fv', $post_id),
+				"leasing_concession" => get_field('samochod_leasing', $post_id),
+				"date_registration"  => get_field('samochod_pierwsza_rejestracja', $post_id),
+				"price" => [
+					'price',
+					get_field('samochod_cena', $post_id),
+					'currency' => 'PLN',
+					'gross_net' => 'gross'
+				],
+				"country_origin" => "pl",
+			];
+
+			$phones = [];
+			foreach(get_field('samochod_telefony', $post_id) as $number){
+				$phones[] = reset($number);
 			}
 
+			$data = [
+				'title' => get_the_title($post_id),
+				'description' => get_field('samochod_opis', $post_id),
+				'city_id' => $city_id,
+				'district_id' => $district_id,
+				'new_used' => get_field('samochod_stan', $post_id),
+				'region_id' => $district_id,
+				'city' => $city,
+				'district' => $district,
+				'category_id' => "29",
+				'municipality' => $municipality,
+				'advertiser_type' => 'business',
+				'contact' => [
+					'person' => 'Przedsiębiorstwo Euro-Kas KIA ' . $municipality,
+					'phone_numbers' => $phones,
+				],
+				'coordinates' => [
+					'latitude' => $latit,
+					'longitude' => $long,
+					'radius' => 0,
+					'zoom_level' => 13,
+				],
+				'params' => $params,
+			];
+
+			$jsoned_data = json_encode($data, JSON_PRETTY_PRINT);
+
+			$oAuthUrl = 'https://www.otomoto.pl/api/open/oauth/token';
+
+			$authArgsArr = ['body' => [
+				'client_id' => get_option('api-key-otomoto-id'),
+				'client_secret' => get_option('api-key-otomoto'),
+				'grant_type' => 'password',
+				'username' => null,
+				'password' => null,
+			]];
+	
+			$accounts = [
+				'used_cars' => ['username' => get_option('used-cars-login'), 'password' => get_option('used-cars-password')],
+				'katowice' => ['username' => get_option('katowice-login'), 'password' => get_option('katowice-password')],
+				'gliwice' => ['username' => get_option('gliwice-login'), 'password' => get_option('gliwice-password')],
+			];
+	
+			foreach ($accounts as $accountName => $accountCredentials) {
+
+				if ($accountCredentials['username']  == get_field('samochod_typogloszenia', $post_id)) {
+					$authArgsArr['body']['username'] = $accountCredentials['username'];
+					$authArgsArr['body']['password'] = $accountCredentials['password'];
+	
+					$response = wp_remote_post($oAuthUrl, $authArgsArr);
+					$responseBody = wp_remote_retrieve_body($response);
+					$decoded = json_decode($responseBody, true);
+					$token = $decoded['access_token'];
+				}
+			}
+
+			$args = array(
+				'headers' => array(
+				'Content-Type'   => 'application/json',
+				'Authorization' => 'Bearer ' . $token
+				),
+				'body'      => $jsoned_data,
+			);
+			$requestUrl = "https://www.otomoto.pl/api/open/adverts";
+
+			if($otomoto_id){
+				$args['method'] = 'PUT';
+				$requestUrl .= $otomoto_id;
+			}else{
+				$args['method'] = 'POST';
+			}
+			
+			$result =  wp_remote_request( $requestUrl, $args );
+			$otomotoUpdatedId = wp_remote_retrieve_body($result)['id'];
+
+			update_field('otomoto_id', $otomotoUpdatedId, $post_id);
+
+			return true;
+		}
+
+
+		function post_new_image_collection()
+		{
+		}
+
+		function add_custom_form()
+		{
+			global $current_screen;
+
+			if ('samochod' != $current_screen->post_type || $current_screen->action == 'add') {
+				return;
+			}
+			$post_id = get_post()->ID;
 			$post = <<<MLS
 <div class="postbox-container" style="margin-top:15px; width: 100%">
 
@@ -158,10 +309,11 @@ class PageTemplaterImport
 	<div class="inside">
 
 		<div class="main">
-
+			<form method="post" method="POST" action="">
 			<p><strong>Wyślij do otomoto:</strong></p>
+			<input type="hidden" value="$post_id" name="post_id" />
 			<button name="upload-otomoto" id="upload-otomoto" class="button button-primary">Zaktualizuj <span style="padding-top:3px" class="dashicons dashicons-arrow-up-alt"></span></button>
-
+			</form>
 
 		</div>
 
@@ -501,21 +653,19 @@ function get_all_adverts($tokens)
 		$cars = $decodedResponse['results'];
 
 		foreach ($cars as $car) {
-			if($car['status'] == 'active'){
+			if ($car['status'] == 'active') {
 				process_custom_post($car);
 			}
 		}
 	}
 }
 
-function add_phone_numbers($phones, $post_id){
+function add_phone_numbers($phones, $post_id)
+{
 
-	foreach($phones as $phone){
+	foreach ($phones as $phone) {
 		add_row('samochod_telefony', ['samochod_telefony_telefon' => $phone], $post_id);
 	}
-
-	
-
 }
 
 function process_custom_post($car)
@@ -529,13 +679,17 @@ function process_custom_post($car)
 	$custom_post['post_title'] = $car['title'];
 	$post_id = wp_insert_post($custom_post);
 
+	$make = isset($car['params']['make']) ? '|' . $car['params']['make'] : '';
+	$model =  isset($car['params']['model']) ? '|' . $car['params']['model'] : '';
+	$version = isset($car['params']['version']) ? '|' . $car['params']['version'] : '';
+
 	// Prepare and insert the custom post meta
 	$meta_keys = array();
 	$meta_keys['otomoto_id'] = $car['id'] ?? '';
 	$meta_keys['samochod_stan'] = $car['new_used'] ?? 'used';
 	$meta_keys['samochod_cena'] = $car['params']['price'][1] ?? 0;
-	$meta_keys['samochod_marka'] = $car['params']['make'] ?? '';
-	$meta_keys['samochod_model'] = '29' . $car['params']['make'] ?? '' . "|" . $car['params']['model'] ?? '' . "|" . $car['params']['version'] ?? '';
+	$meta_keys['samochod_marka'] = '29' . $make . $model . $version;
+	$meta_keys['samochod_model'] = $car['params']['model'];
 	$meta_keys['samochod_rok'] = $car['params']['year'] ?? '';
 	$meta_keys['samochod_przebieg'] = $car['params']['mileage'] ?? '';
 	$meta_keys['samochod_pojemnosc'] = $car['params']['engine_capacity'] ?? '';
@@ -547,9 +701,7 @@ function process_custom_post($car)
 	$meta_keys['samochod_vin'] = $car['params']['vin'] ?? '';
 	$meta_keys['samochod_liczba_drzwi'] = $car['params']['door_count'] ?? '';
 	$meta_keys['samochod_liczba_miejsc'] = $car['params']['nr_seats'] ?? '';
-
 	$meta_keys['samochod_pierwsza_rejestracja'] = $car['params']['date_registration'] ?? '';
-
 	$meta_keys['samochod_finansowanie'] = $car['params']['financial_option'] ?? '';
 
 	$meta_keys['samochod_fv'] = $car['params']['vat'] ?? '';
@@ -560,6 +712,16 @@ function process_custom_post($car)
 	$meta_keys['samochod_opis'] = $car['description'] ?? '';
 
 	$meta_keys['samochod_lokalizacja'] = strtolower($car['city']['pl']);
+
+	if ($car['params']['metallic'] == "1") {
+		$meta_keys['samochod_kolor_typ'] == "metallic";
+	} else if ($car['params']['pearl'] == "1") {
+		$meta_keys['samochod_kolor_typ'] == "pearl";
+	} else if ($car['params']['matt'] == "1") {
+		$meta_keys['samochod_kolor_typ'] == "matt";
+	} else {
+		$meta_keys['samochod_kolor_typ'] == "acrylic";
+	}
 
 	$custom_fields = array();
 	$place_holders = array();
@@ -573,7 +735,7 @@ function process_custom_post($car)
 	$wpdb->query($wpdb->prepare("$query_string ", $custom_fields));
 
 	$phones = $car['contact']['phone_numbers'] ?? [];
-	if(count($phones)){
+	if (count($phones)) {
 		add_phone_numbers($phones, $post_id);
 	}
 
@@ -581,23 +743,22 @@ function process_custom_post($car)
 	if (isset($car['photos'])) {
 
 		foreach ($car['photos'] as $photo) {
-			if(count($photo) > 0){
-				if(isset($photo['1080x720'])){
+			if (count($photo) > 0) {
+				if (isset($photo['1080x720'])) {
 					$image_ids[] = download_image_from_url($photo['1080x720']);
-				}else{
+				} else {
 					$image_ids[] = download_image_from_url(reset($photo));
 				}
 			}
 		}
 
-		if(count($image_ids)){
+		if (count($image_ids)) {
 			set_post_thumbnail($post_id, $image_ids[0]);
 		}
-		
+
 		foreach ($image_ids as $attachement_id) {
 			attach_image_to_post($attachement_id, $post_id);
 		}
-
 	}
 
 	return true;
